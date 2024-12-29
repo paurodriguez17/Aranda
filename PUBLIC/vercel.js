af01744310c34c3f08a6e8dbf8131064e3df932e
paurodriguez17 <paurodriguez170@gmail.com>
2024-12-28 23:37:40 -0300

    cambiosss

--- PUBLIC/vercel.js
+++ PUBLIC/vercel.js
@@ -2,14 +2,14 @@
     "version"; 2,
     "builds";[
       {
-        "src": "/inicio/inicio.js", // Cambia "index.js" al nombre de tu archivo principal de servidor
+        "src": "puyblic/inicio/inicio.js", // Cambia "index.js" al nombre de tu archivo principal de servidor
         "use": "@vercel/node"
       }
     ],
     "routes"; [
       {
         "src": "/(.*)",
-        "dest": "/inicio/inicio.js" // Cambia al nombre de tu archivo principal
+        "dest": "public/inicio/inicio.js" // Cambia al nombre de tu archivo principal
       }
     ]
   }
